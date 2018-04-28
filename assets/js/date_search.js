57b9484de3059b59f22831ea6cb0f2291a0b091a
grat2 <gf310031414@gmail.com>
2018-04-28 12:26:40 -0700

    did some stuff with the search function

--- assets/js/date_search.js
+++ assets/js/date_search.js
@@ -7,7 +7,11 @@ function getDate(file) {
     var foundDate = false;
     var foundAssignment = false;
     var str;
+    var assignment;
+    var date = [];
     var assignmentWords = ["assignment", "project", "homework", "problem", "question"];
+    var dateNums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
+                    "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
     for(str in textByLine) {
         line = str.split(" ");
         var word, compWord;
@@ -15,6 +19,26 @@ function getDate(file) {
             for(compWord in assignmentWords) {
                 if(compWord == word) {
                     foundAssignment = true;
+                    assignment = word;
+                }
+            }
+            word = word.split("/");
+            if(word.length == 3 && foundAssignment == false) {
+                var i;
+                //get date from word if a date is found
+                for(i = 0; i < 12; i++) {
+                    if(word[0] == dateNums[i]) {
+                        date[0] = word[0];
+                        break;
+                    }
+                }
+                for(i in dateNums) {
+                    if(word[1] == i) {
+                        date[1] = word[1];
+                    }
+                    if(word[2] == i) {
+                        date[2] = word[2];
+                    }
                 }
             }
         }
