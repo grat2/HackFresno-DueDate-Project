b56e2af96f88fd0cf80c7567a6455e3d97b4347b
grat2 <gf310031414@gmail.com>
2018-04-28 14:41:51 -0700

    added loop to create all events found in webpage

--- extension/background.js
+++ extension/background.js
@@ -5,37 +5,41 @@
 // Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
 // stored credentials.
 
-var event = {
-  'summary': createevent[1][0],
-  'start': {
-    'dateTime': createevent[1][1] + 'T09:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'end': {
-    'dateTime': createevent[1][1] + 'T10:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'}
-  ],
-  'reminders': {
-    'useDefault': false,
-    'overrides': [
-      {'method': 'popup', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 60}
-    ]
-  }
-};
 
-var request = gapi.client.calendar.events.insert({
-  'calendarId': 'primary',
-  'resource': event
-});
+//grabs every event in the createevent array and stores it in eData for conversion
+//to a google calendar event
+var eData;
+for(eData in createevent) {
+    var event = {
+        'summary': eData[0],
+        'start': {
+            'dateTime': eData[1] + 'T09:00-07:00',
+        },
+        'end': {
+            'dateTime': eData[1] + 'T09:30-07:00',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'}
+        ],
+        'reminders': {
+            'useDefault': false,
+            'overrides': [
+                {'method': 'popup', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 60}
+            ]
+        }
+    };
 
-request.execute(function(event) {
-  appendPre('Event created: ' + event.htmlLink);
-});
+    var request = gapi.client.calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event
+    });
+
+    request.execute(function(event) {
+        appendPre('Event created: ' + event.htmlLink);
+    });
+}
